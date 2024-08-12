0bf1b8f85e4148d1ad47cee5ceef9e8589a2f88f
sergi <sergifusterdura@gmail.com>
2024-08-12 14:57:20 +0200

    fix: import mismatch

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-from src.models import fullnet
+from src.models.models import *
 from src.utils import utils as u
 
 """ model = fullnet.FullNetWrapper()
 model.train('18', epochs=100)
 model.save('src/experiments/fullnet/experiment_0/models', 'test.tar') """
-model = fullnet.FullNetWrapper('src/experiments/fullnet/experiment_0/models/test.pth')
+model = FullNetWrapper('src/experiments/fullnet/experiment_0/models/test.pth')
 
 master, *(_) = u.extract_central_patch(u.extract_s2mat('data/images/mat/S2/18.mat'), 1024)
 slave, *(_) = u.extract_central_patch(u.extract_s3mat('data/images/mat/S3/18.mat'), 1024//15)
