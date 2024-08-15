1563929eb67833aa197e22694fa37bfa0b4b8bc7
SergiFuster <al376646@uji.es>
2024-08-15 11:55:49 +0200

    fix: little fix on input of network.
    Input size 366 was not 16 divisible then was mandatory extract a
    patch of 352.

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ routes = u.get_files_path('data/images/S3')
 for i in range(5):
     samples = 2**i
     chosen = np.random.choice(routes, samples, replace=False)
-    images = np.array([u.extract_s3mat(route) for route in chosen])
+    images = np.array([u.extract_central_patch(u.extract_s3mat(route), 352) for route in chosen])
     images = np.moveaxis(images, -1, 1)
     model = RuNetv2()
     model.train(images, epochs=1000, learning_rate=0.0001, upsamplings=2)
